28cccb6a6426d28c8c44f0634154e11fd76924ff
rmarquesnovaenergia <rmarques@novaenergia.com.br>
2023-06-19 15:56:04 -0300

    sidebar completo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,27 @@
+import { PencilLine } from 'phosphor-react'
+
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
   return(
     <aside className={styles.sidebar}>
  
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1681207405529-34174a1090f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="" />
+      <img 
+      className={styles.cover} 
+      src="https://images.unsplash.com/photo-1681207405529-34174a1090f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="" 
+      />
+
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://www.github.com.br/rafaelmvale.png"/> 
+        
         <strong>Rafael Marques</strong>
         <span>Web Dev</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar Perfil
         </a>
       </footer>
